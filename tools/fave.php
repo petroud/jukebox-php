837a6407615f79e3bb1a275bd3b74e88ff5a6cd7
petroud <dpetrou1@outlook.com>
2021-11-15 09:35:30 +0200

    Concerts page completed along with favorites button

--- tools/fave.php
+++ tools/fave.php
@@ -8,19 +8,17 @@
     $idconcert = $_GET['id'];
     $userID = $_SESSION['user_id'];
 
+
     $checkQuery = "SELECT * FROM favorites WHERE user_id = $userID AND concert_id = $idconcert";
    
     $result = mysqli_query($con, $checkQuery); 
-    if(is_null($result)){
+    if($result && mysqli_num_rows($result)===1){
         $delQuery = "DELETE FROM favorites WHERE user_id = $userID AND concert_id = $idconcert";
         mysqli_query($con, $delQuery);
     }else{
         $addQuery = "INSERT INTO favorites(id,user_id,concert_id) values('$userID". $idconcert ."', '$userID','$idconcert')";
         mysqli_query($con, $addQuery);
     }
+    header("Location: ../concerts.php");
 
-    
-    $queryStruct = "UPDATE users SET confirmed = NOT confirmed WHERE id='$idToChangeConfState'";
-    mysqli_query($con,$queryStruct);
-    header("Location: ../administration.php");
 ?>
