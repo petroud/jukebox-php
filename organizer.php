1e676c9b1a25242f58b492642219c3d6210c8320
petroud <dpetrou1@outlook.com>
2021-11-18 23:43:36 +0200

    Organizer page completed

--- organizer.php
+++ organizer.php
@@ -16,17 +16,21 @@ session_start();
     <title>Jukebox | Users Console</title>
     <link rel="shortcut icon" type="image/png" href="assets/favicon.png">
     <link rel="stylesheet" href="/src/organizer.css">
+    <link rel="stylesheet" href="/src/tablesorter.css">
+    <link rel="stylesheet" href="/src/editor.css">
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@500&family=Work+Sans:ital,wght@0,100;0,200;0,400;0,500;0,600;1,100&display=swap" rel="stylesheet">
+   
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
 
     <script src="/src/welcome.js"></script>
+    <script src="/src/tablesort.js"></script>
+    <script src="/src/organizer.js"></script>
+
+
 
     <style>
-        .wrapper{
-            width: 600px;
-            margin: 0 auto;
-        }
         body {
           background-image: url('cdn/background.jpg');
           background-repeat: no-repeat;
@@ -37,17 +41,28 @@ session_start();
           overflow:auto;
         }
         </style>
-     <script>
-        $(document).ready(function(){
-            $('[data-toggle="tooltip"]').tooltip();   
-        });
-    </script>
 </head>
 
 
 
-<body>
-    <div class="banner">
+<body> 
+    <div class="overlay" id="editorback"> 
+        <div class="editor-box" id="editor-box">
+            <h1><span class="waving">Edit Concert Information</span></h1>
+                    <form method="post" class="editor-form">
+                        <input name = "title" type="text" class = "input-box" placeholder= "Concert Title" id="edit_title">
+                        <input name = "artist" type="text" class = "input-box" placeholder= "Artist Name" id="edit_artist" >
+                        <input name = "date" type="date" class = "input-box" placeholder= "Date" id="edit_date">
+                        <input name = "genre" type="text" class = "input-box" placeholder= "Genre" id="edit_genre">
+                        <input type="hidden" name="id" value="" id="key_field">
+                        <button type="button" class="button" onclick="submitEdits()">Submit</button>
+                        <button type="button" class="button btn-2 repos" onclick="closeEditor()" id="exitbtn">Cancel</button>
+                        <div class="resultBox" id="resBox"><p class="result-msg" id="resMsg"></p></div>
+                    </form>
+        </div>
+    </div> 
+
+    <div class="banner" id="banner">
             <div class="navbar">
                 <div class="logo">
                   <a href="index.php"><img src="cdn/logo.png"></a>
@@ -104,10 +119,12 @@ session_start();
 
             </div>
 
-    <div class="content">
-                <div><h1><span>My Concerts and Events</span></h1></div>
-
-                    <?php
+    <div class="content" id="content"> 
+                <div><h1><span>My Concerts and Events</span></h1>
+                    <button onclick ="addConcert()" class="add-button"><img src="/assets/filter.png" alt="Filter Concerts"></button>
+                </div>
+                              
+            <?php
                     // Include config file
                     require_once "connection.php";
                     $uid = $_SESSION['user_id'];
@@ -115,7 +132,7 @@ session_start();
 
                     if($result = mysqli_query($con, $sql)){
                         if(mysqli_num_rows($result) > 0){
-                            echo '<table class="content-table">';
+                            echo '<table class="content-table table-sortable">';
                                 echo "<thead>";
                                     echo "<tr>";
                                         echo "<th>ID</th>";
@@ -129,16 +146,15 @@ session_start();
                                 echo "</thead>";
                                 echo "<tbody>";
                                 while($row = mysqli_fetch_array($result)){
-                                    echo "<tr>";
+                                    echo '<tr id="row_'.$row['id'].'">';
                                         echo "<td>" . $row['id'] . "</td>";
-                                        echo "<td>" . $row['title'] . "</td>";
+                                        echo '<td id="title_'.$row['id'].'">'. $row['title'] . '</td>';
                                         echo "<td>" . $row['date'] . "</td>";
                                         echo "<td>" . $row['artistname'] . "</td>";
                                         echo "<td>" . $row['category'] . "</td>";
                                         echo "<td>";
-                                            echo '<a href="/tools/confedit.php?id='. $row['id'] .'"><img class = "conf-ico" src="/assets/activate.png" alt="change confirmed"></a>';
-                                            echo '<a href="/tools/update.php?id='. $row['id'] .'"><img class = "conf-ico" src="/assets/editing.png" alt="change confirmed"></a>';
-                                            echo '<a href="/tools/userdel.php?id='. $row['id'] .'"><img class = "conf-ico" src="/assets/bin.png" alt="change confirmed"></a>';
+                                            echo '<a href="javascript:editConcert('.$row['id'].')"><img class = "conf-ico" src="/assets/editing.png" alt="edit concert"></a>';
+                                            echo '<a href="javascript:delConcert('.$row['id'].')"><img class = "conf-ico" src="/assets/bin.png" alt="delete concert"></a>';
                                         echo "</td>";
                                     echo "</tr>";
                                 }
@@ -147,7 +163,7 @@ session_start();
                             // Free result set
                             mysqli_free_result($result);
                         } else{
-                            echo '<div class="alert alert-danger"><em>No records were found.</em></div>';
+                            echo '<div><em>No records were found.</em></div>';
                         }
                     } else{
                         echo "Something went wrong. Please try again later.";
