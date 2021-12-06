0dcacfb87b6053fb5fff48e9ec9c31194f6747e1
Dimitrios Petrou <dpetrou1@outlook.com>
2021-12-07 01:32:35 +0200

    Loading and IDM setup page created

---
+++ loading.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+
+<head>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta charset="utf-8">
+    <title>Warming up the engine...</title>
+    <link rel="shortcut icon" type="image/png" href="assets/favicon.png">
+    <link rel="stylesheet" href="/src/main.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@500&family=Work+Sans:ital,wght@0,100;0,200;0,400;0,500;0,600;1,100&display=swap" rel="stylesheet">
+
+
+    <style>
+        body {
+          background-color: rgb(255,255,255);      
+          font-family: "Roboto",sans-serif;
+          font-family: 'Work Sans', sans-serif;
+        }
+        </style>
+</head>
+
+
+<body>
+    <div class="loading centered waving-text">
+        <img src="./assets/loader.gif" alt="loader" class ="centered-loader">
+        <h2>JUKEBOX is now starting up. Thank you for being patient...</h2>
+    </div>
+</body>
---
+++ setup.php
@@ -0,0 +1,32 @@
+<?php
+
+
+?>
+
+<!DOCTYPE html>
+
+<head>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta charset="utf-8">
+    <title>Jukebox | IDM Setup</title>
+    <link rel="shortcut icon" type="image/png" href="assets/favicon.png">
+    <link rel="stylesheet" href="/src/main.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@500&family=Work+Sans:ital,wght@0,100;0,200;0,400;0,500;0,600;1,100&display=swap" rel="stylesheet">
+    <style>
+        body {
+          background-color: rgb(255,255,255);      
+          font-family: "Roboto",sans-serif;
+          font-family: 'Work Sans', sans-serif;
+        }
+        </style>
+</head>
+
+
+<body>
+    <div class="loading centered waving-text">
+        <img src="./assets/gears.gif" alt="loader" class ="centered-gear">
+        <h2>There is nothing to see here. Everything is being automatically configured...</h2>
+    </div>
+</body>
