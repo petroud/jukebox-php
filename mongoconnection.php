8031007572804b50d1db636e94b2deaa698bb906
Dimitrios Petrou <dpetrou1@outlook.com>
2021-12-14 04:34:02 +0200

    Minor

--- mongoconnection.php
+++ mongoconnection.php
@@ -1,6 +1,6 @@
 <?php
     require 'config/vendor/autoload.php';
-    $conn = new MongoDB\Client("mongodb://admin:admin@192.168.1.11:27017");
+    $conn = new MongoDB\Client("mongodb://admin:admin@192.168.1.11:27018");
     $db = $conn->jukebox;
 
     $users = $db->users;
