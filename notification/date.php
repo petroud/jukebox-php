0826aa1dd82ec2ec506da376ea692eb9b6511ac0
Dimitrios Petrou <dpetrou1@outlook.com>
2021-12-18 16:09:21 +0200

    Unneeded files deleted

--- notification/date.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-$datetime1 = new DateTime('2021-12-15 22:13:06');//start time
-$datetime2 = new DateTime('2021-12-21 11:55:09');//end time
-$interval = $datetime1->diff($datetime2);
-echo $interval->format('%d days %H hours %i minutes %s seconds');
