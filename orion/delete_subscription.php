e147f44995a2ea3a6f46e3d669438bd42662dafa
Dimitrios Petrou <dpetrou1@outlook.com>
2021-12-19 02:48:43 +0200

    Uneeded dependecies deleted

--- orion/delete_subscription.php
+++ orion/delete_subscription.php
@@ -1,13 +1,11 @@
 <?php
 session_start();
 require_once "../functions.php";
-require_once "../connection.php";
 include("../parser.php");
 include("subscription_utils.php");
 check_login();
 check_user();
 
 $cid = $_POST['cid'];
-$uid = $_SESSION['user_id'];
 
-removeOrionSubscription($cid,$con);
+removeOrionSubscription($cid);
